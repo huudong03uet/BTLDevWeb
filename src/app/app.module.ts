7a4cbd5ebee81020320c3902c434e607526dbb8d
dquynh2811 <21020087@vnu.edu.vn>
2023-09-03 02:24:16 +0700

    add editor and live-preview functionality with CodeMirror

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,18 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
+import { EditorComponent } from './editor/editor.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    EditorComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/editor/editor.component.ts
@@ -0,0 +1,74 @@
+import { Component, ViewChild, ElementRef, AfterViewInit } from '@angular/core';
+
+declare let CodeMirror: any;
+
+@Component({
+  selector: 'app-editor',
+  templateUrl: './editor.component.html',
+  styleUrls: ['./editor.component.css']
+})
+
+export class EditorComponent implements AfterViewInit {
+  @ViewChild('htmlTextarea') htmlTextarea!: ElementRef;
+  @ViewChild('cssTextarea') cssTextarea!: ElementRef;
+  @ViewChild('jsTextarea') jsTextarea!: ElementRef;
+  @ViewChild('outputFrame') outputFrame!: ElementRef;
+
+  htmlEditor!: any;
+  cssEditor!: any;
+  jsEditor!: any;
+
+  ngAfterViewInit() {
+    this.htmlEditor = CodeMirror.fromTextArea(this.htmlTextarea.nativeElement, {
+      mode: 'xml',
+      lineNumbers: true,
+      theme: 'monokai',
+      autoCloseTags: true  // Bật chức năng autoCloseTags
+    });
+    this.htmlEditor.on('change', () => this.run());
+
+    this.cssEditor = CodeMirror.fromTextArea(this.cssTextarea.nativeElement, {
+      mode: 'css',
+      lineNumbers: true,
+      theme: 'monokai',
+      autoCloseTags: true  // Bật chức năng autoCloseTags
+    });
+    this.cssEditor.on('change', () => this.run());
+
+    this.jsEditor = CodeMirror.fromTextArea(this.jsTextarea.nativeElement, {
+      mode: 'javascript',
+      lineNumbers: true,
+      theme: 'monokai',
+      autoCloseTags: true  // Bật chức năng autoCloseTags
+    });
+    this.jsEditor.on('change', () => this.run());
+  }
+
+  run() {
+    const htmlCode = this.htmlEditor.getValue();
+    const cssCode = this.cssEditor.getValue();
+    const jsCode = this.jsEditor.getValue();
+    const output = this.outputFrame.nativeElement;
+
+    // Hiển thị HTML và CSS trong iframe
+    output.contentDocument.body.innerHTML = htmlCode;
+    const styleTag = document.createElement('style');
+    styleTag.innerHTML = cssCode;
+    output.contentDocument.head.appendChild(styleTag);
+
+    // Chạy JavaScript
+    const scriptTag = document.createElement('script');
+    scriptTag.innerHTML = jsCode;
+    output.contentDocument.body.appendChild(scriptTag);
+  }
+
+  public isMenuOpen: boolean = false;
+
+  openMenu(): void {
+    this.isMenuOpen = true;
+  }
+
+  closeMenu(): void {
+    this.isMenuOpen = false;
+  }
+}
