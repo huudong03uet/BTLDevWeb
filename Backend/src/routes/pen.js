af48aeeee666cf6f33ed720d2a5330e09d426bf5
nguyenvankhang <21020768@vnu.edu.vn>
2023-10-17 10:47:36 +0700

    abc

--- Backend/src/routes/pen.js
+++ Backend/src/routes/pen.js
@@ -1,8 +1,10 @@
 import express from "express";
 import penController from "../controllers/penController"
+
 const router = express.Router();
 
 router.post("/getPenById", penController.getPenById);
 router.post("/createOrUpdatePen", penController.createOrUpdatePen);
 router.get("/getInfoPen/:id", penController.getInfoPen);
+
 module.exports = router;
