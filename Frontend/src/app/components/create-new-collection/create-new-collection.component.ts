a7b391556e96cda2c43cd337ee1025511fda6255
dquynh2811 <21020087@vnu.edu.vn>
2023-12-27 00:25:44 +0700

    fix some tiny bug in Create New Collection Form

--- Frontend/src/app/components/create-new-collection/create-new-collection.component.ts
+++ Frontend/src/app/components/create-new-collection/create-new-collection.component.ts
@@ -1,37 +1,39 @@
-import { Component, EventEmitter, Input, Output } from '@angular/core';
-// template: `
-// <create-new-collection [hiddenCreateNewCollection]="hiddenCreateNewCollection" (closeCreateNewCollection)="closeCreateNewCollection()"></create-new-collection>
-// `
-// hiddenCreateNewCollection: boolean = true;
-// openCreateNewCollection() {
-// this.hiddenCreateNewCollection = false;
-// }
+import { Component, EventEmitter, Output } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
-// closeCreateNewCollection() {
-// this.hiddenCreateNewCollection = true;
-
-
-// <button type="button" class="btn-close-new-collection" (click)="closeCreateNewCollection()">
 @Component({
   selector: 'app-create-new-collection',
   templateUrl: './create-new-collection.component.html',
   styleUrls: ['./create-new-collection.component.scss', '../../pages/settings/style-settings.scss']
 })
 export class CreateNewCollectionComponent {
-
   @Output() close = new EventEmitter<void>();
 
-  
+  createForm: FormGroup;
+  maxCharacterLimit = 1000;
+
+  constructor(private fb: FormBuilder) {
+    this.createForm = this.fb.group({
+      collectionTitle: ['', [Validators.required, Validators.maxLength(this.maxCharacterLimit)]],
+      collectionDescription: ['', [Validators.maxLength(this.maxCharacterLimit)]],
+    });
+  }
+
   onCloseCreateNewCollection() {
     this.close.emit();
   }
 
-}
+  onSubmit() {
+    if (this.createForm.valid) {
+      // Implement your logic to create a new collection
+      console.log('Title:', this.createForm.value.collectionTitle);
+      console.log('Description:', this.createForm.value.collectionDescription);
 
-// ,
-  // template: `
-  // <button type="button" class="btn-close-new-collection" (click)="onCloseCreateNewCollection()">
-  //     <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 100 100"><path d="M96.8 83.7 63.1 50l33.7-33.7c3.6-3.6 3.6-9.4 0-13.1s-9.5-3.6-13.1 0L50 36.9 16.3 3.2C12.7-.4 6.9-.4 3.2 3.2s-3.6 9.5 0 13.1L36.9 50 3.2 83.7c-3.6 3.6-3.6 9.4 0 13.1s9.5 3.6 13.1 0L50 63.1l33.7 33.7c3.6 3.6 9.4 3.6 13.1 0s3.6-9.5 0-13.1z"></path></svg>
-  // </button>
-  
-// `
+      // Optionally, you can reset the form fields
+      this.createForm.reset();
+
+      // Close the form
+      this.onCloseCreateNewCollection();
+    }
+  }
+}
