6e96539653deba5719178c88a75310fbc1e45d9e
Nguyễn Hữu Đồng <21020760@vnu.edu.vn>
2023-12-28 02:36:57 +0700

    done your work

--- Frontend/src/app/pages/your-work/your-work-deleted/your-work-deleted.component.ts
+++ Frontend/src/app/pages/your-work/your-work-deleted/your-work-deleted.component.ts
@@ -6,5 +6,86 @@ import { Component } from '@angular/core';
   styleUrls: ['./your-work-deleted.component.scss']
 })
 export class YourWorkDeletedComponent {
-
+  list_deleted = [
+    {
+      "id": 1,
+      "name": "Chưa đặt tên",
+      "type": "pen"
+    },
+    {
+      "id": 2,
+      "name": "hú hú hú",
+      "type": "pen"
+    },
+    {
+      "id": 3,
+      "name": "Chưa đặt tên",
+      "type": "pen"
+    },
+    {
+      "id": 4,
+      "name": "hế hế hế",
+      "type": "project"
+    },
+    {
+      "id": 5,
+      "name": "Chưa đặt tên",
+      "type": "pen"
+    },
+    {
+      "id": 6,
+      "name": "Chưa đặt tên",
+      "type": "pen"
+    },
+    {
+      "id": 7,
+      "name": "Chưa đặt tên",
+      "type": "pen"
+    },
+    {
+      "id": 8,
+      "name": "Chưa đặt tên",
+      "type": "project"
+    },
+    {
+      "id": 9,
+      "name": "Chưa đặt tên",
+      "type": "pen"
+    },
+    {
+      "id": 10,
+      "name": "Chưa đặt tên",
+      "type": "project"
+    },
+    {
+      "id": 11,
+      "name": "Chưa đặt tên",
+      "type": "pen"
+    },
+    {
+      "id": 12,
+      "name": "Chưa đặt tên",
+      "type": "collection"
+    },
+    {
+      "id": 13,
+      "name": "Chưa đặt tên",
+      "type": "pen"
+    },
+    {
+      "id": 14,
+      "name": "Chưa đặt tên",
+      "type": "collection"
+    },
+    {
+      "id": 15,
+      "name": "Chưa đặt tên",
+      "type": "pen"
+    },
+    {
+      "id": 16,
+      "name": "Chưa đặt tên",
+      "type": "collection"
+    }
+  ]
 }
