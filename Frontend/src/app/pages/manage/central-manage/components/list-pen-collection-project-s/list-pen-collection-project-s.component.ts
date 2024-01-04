aead5d687c555b2115386cc926b9b95dbec84514
Nguyễn Hữu Đồng <21020760@vnu.edu.vn>
2024-01-04 16:37:36 +0700

    fix forget password

--- Frontend/src/app/pages/manage/central-manage/components/list-pen-collection-project-s/list-pen-collection-project-s.component.ts
+++ Frontend/src/app/pages/manage/central-manage/components/list-pen-collection-project-s/list-pen-collection-project-s.component.ts
@@ -1,6 +1,7 @@
 import { Component, Input, OnInit } from '@angular/core';
 import { HostService } from 'src/app/host.service';
 import axios from 'axios';
+import { max } from 'rxjs';
 
 @Component({
   selector: 'app-list-pen-collection-project-s',
@@ -14,6 +15,7 @@ export class ListPenCollectionProjectSComponent implements OnInit {
   @Input() type: string = 'pen';
   
   @Input() datas: any;
+  max_item_per_page: number = 10;
 
   constructor(
     private myService: HostService,
@@ -29,16 +31,62 @@ export class ListPenCollectionProjectSComponent implements OnInit {
     } else if (this.type == 'project') {
 
       return;
-      apiUrl = this.myService.getApiHost() + `/collection/getAllCollection?attr_sort=${this.attr_sort}&order_by=${this.order_by}&deleted=${this.deleted}`;
     }
 
+    
     console.log(apiUrl);
     
 
     axios.get(apiUrl).then((response) => {
       this.datas = response.data;
+
+      this.pen_ids_current = this.datas.slice(0, this.max_item_per_page);
+      this.check_is_start_end();
     }).catch((error) => {
       console.error('Error:', error);
     });
+  }
+
+
+
+  page_now: number = 1;
+  pen_ids_current: any[] = [];
+  is_end: boolean = false;
+  is_start: boolean = true;
+
+
+  check_is_start_end() {
+    if (this.page_now == 1) {
+      this.is_start = true;
+    } else {
+      this.is_start = false;
+    }
+
+    if (this.page_now * this.max_item_per_page >= this.datas.length) {
+      this.is_end = true;
+    } else {
+      this.is_end = false;
+    }
+  }
+  
+
+
+  // ngOnChanges() {
+    ngOnChanges() {
+      this.pen_ids_current = this.datas.slice(0, this.max_item_per_page);
+      this.check_is_start_end();
+    }
+
+
+  clickNextPageButton() {
+    this.page_now += 1;
+    this.pen_ids_current = this.datas.slice((this.page_now - 1) * this.max_item_per_page, this.page_now * this.max_item_per_page);
+    this.check_is_start_end();
+  }
+
+  clickPrevPageButton() {
+    this.page_now -= 1;
+    this.pen_ids_current = this.datas.slice((this.page_now - 1) * this.max_item_per_page, this.page_now * this.max_item_per_page);
+    this.check_is_start_end();
   }
 }
